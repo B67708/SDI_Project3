605a935a4daa0c6206d31bfc9bd98d92a398a681
John Cardiff-Winchell <johnwinchell86@fullsail.edu>
2013-02-21 19:06:51 -0500

    Added json.js

---
+++ json.js
@@ -0,0 +1,76 @@
+// John Cardiff-Winchell
+//SDI 1302
+//Project 3 json
+
+var json = {
+"restaurant": {
+	"pizzaHut":{
+		"appetizer": "Mozzarella sticks",
+		"mainDish": "Pizza",
+		"drink": "Mountain dew"
+				},
+		
+		
+	"applebees":{ 
+		"appetizer": "Boneless buffalo wings",
+		"mainDish": "Steak and shrimp",
+		"drink": "Mountain dew"
+				},
+				
+	"tonys":{ 
+		"appetizer": "Bacon",
+		"mainDish": "Steak Sandwich",
+		"drink": "Mountain dew"
+				},
+				
+	"donPablos":{ 
+		"appetizer": "Nacho",
+		"mainDish": "Chicken Fajita",
+		"drink": "Mountain dew"
+				},
+				
+	"bennigans":{ 
+		"appetizer": "Breaded Mushrooms",
+		"mainDish": "Burger",
+		"drink": "Mountain dew"
+				},
+	}	
+};
+
+
+var json2 = 
+{
+"restaurant": [
+	{
+		"place":"Pizza Hut",
+		"appetizer": "Mozzarella sticks",
+		"mainDish": "Pizza",
+		"drink": "Mountain dew"
+				},
+		
+		{
+	"place":"Applebee's", 
+		"appetizer": "Boneless buffalo wings",
+		"mainDish": "Steak and shrimp",
+		"drink": "Mountain dew"
+				},
+			{	
+	"place":"Tony's",
+		"appetizer": "Bacon",
+		"mainDish": "Steak Sandwich",
+		"drink": "Mountain dew"
+				},
+				{
+	"place":"Don Pablo's",
+		"appetizer": "Nacho",
+		"mainDish": "Chicken Fajita",
+		"drink": "Mountain dew"
+				},
+				{
+	"place":"Bennigan's",
+		"appetizer": "Breaded Mushrooms",
+		"mainDish": "Burger",
+		"drink": "Mountain dew"
+				},
+	]	
+}
